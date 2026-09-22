fff72ff1a8d960c1ad7971785c2d03c09d82072b
devarajc2401-eng <devarajc2401@gmail.com>
2026-09-22 13:03:22 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+//creating a rectangle class and calculating area of rectangle 
+public class Rectangle {
+    int length;
+    int breadth;
+
+    void CalculateArea() {
+        int area = length*breadth;
+        System.out.println("Area of Rectangle :"+area);
+    }
+    public static void main(String[] args){
+        Rectangle r = new Rectangle();
+        r.length =10;
+        r.breadth=5;
+        r.CalculateArea();
+    }
+}
